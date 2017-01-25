ea257bdc9a688cb04207c6dfa29da555230e458d
RickardSjogren <r.sjogren89@gmail.com>
2017-01-25 13:06:07 +0100

    Simplification of PCA plotting, time-series residuals removed.

--- airgard/processing.py
+++ airgard/processing.py
@@ -3,11 +3,13 @@ from datetime import timedelta
 
 import numpy as np
 import pandas as pd
+from matplotlib import dates
+from matplotlib import pyplot as plt
 
-from signalprocessing_utils.misc import chunk_df_on_diff
-from signalprocessing_utils.preprocessing import _partial_w_name, fft_amplitude
 from signalprocessing_utils import modelling
 from signalprocessing_utils import plotting
+from signalprocessing_utils.misc import chunk_df_on_diff
+from signalprocessing_utils.preprocessing import _partial_w_name, fft_amplitude
 
 
 def process_airgard_df(df, start_day, spectral_transform=None,
@@ -93,7 +95,7 @@ def process_airgard_df(df, start_day, spectral_transform=None,
     return processed_df
 
 
-def fit_and_plot(data, sample_start, sample_end, n_components=5, verbose=True):
+def fit_and_plot(data, sample_end, n_components=5, verbose=True, copy=True):
     """ Fit an n-component PCA-model of subset of processed
      Airgard-data and plot control charts.
 
@@ -101,8 +103,6 @@ def fit_and_plot(data, sample_start, sample_end, n_components=5, verbose=True):
     ----------
     data : pandas.DataFrame
         Data to modell and plot.
-    sample_start : Any
-        Index of start observation.
     sample_end : Any
         Index of sample end observation
     n_components : int
@@ -115,28 +115,45 @@ def fit_and_plot(data, sample_start, sample_end, n_components=5, verbose=True):
     matplotlib.pyplot.Figure
     numpy.ndarray[matplotlib.pyplot.Axes]
     """
-    data = data[~np.isnan(data).any(1)]
-    week_data = data[sample_start: sample_end]
+    index = pd.DataFrame(index=data.index)
+
+    if copy:
+        data = data.dropna(inplace=False)
+    else:
+        data.dropna(inplace=True)
+
+    subsample = data[:sample_end]
     if verbose:
         print('Fit {}-component PCA-model'.format(n_components))
 
-    pca = modelling.PCAPipeline(n_components=n_components).fit(week_data)
+    pca = modelling.PCAPipeline(n_components=n_components).fit(subsample)
     if verbose:
         print('Project all observations.')
-    scores = pca.transform(data[sample_start:])
+    scores = pca.transform(data)
 
     if verbose:
         print('Calculate observation residuals.')
-    residuals = pca.residual_sum_of_squares(data[sample_start:], scores)
+
+    rss = pca.residual_sum_of_squares(data, scores)
 
     if verbose:
-        print('Calculate Hotelling\'s T2')
-    hotellings_t2 = pca.hotellings_t2(scores)
+        print('Joining back missing values.')
+    scores = index.join(scores)
+    rss = index.join(rss)
 
     print('Plot results.')
-    f, axes = plotting.plot_pca_controll_charts(pca, None, scores,
-                                                residuals.values, hotellings_t2,
-                                                figsize=(10, 12), sigma=6)
+    f, axes = plt.subplots(n_components + 1, 1, sharex=True, figsize=(10, 12))
+    times = dates.date2num(index.index.to_pydatetime())
+
+    plotting.plot_pca_controll_charts(pca, scores, rss, times,
+                                      axes=axes, sigma=6)
+
+    for ax in axes:
+        ax.axvline(times[len(subsample)], linestyle=':', color=(.2, .2, .2))
+
+    plotting.make_ax_timespan(axes[-1], index.index.to_pydatetime())
+    f.tight_layout()
+
     return f, axes
 
 
@@ -168,3 +185,68 @@ def collapse_spectrum(data, n=10):
 
     data = pd.concat((data, binned_spectrum), axis=1)
     return data
+
+
+def airgard_plot_and_spectrogram(spectrum, df, diff_n=100,
+                                 figsize=(11, 12.6), dpi=800):
+    """ Make plots of derivative of accelerometers, current and spectrogram.
+
+    Parameters
+    ----------
+    spectrum : array_like
+        Spectrum
+    df : pandas.DataFrame
+        Airgard-data frame with columns Cur, X, Y, Z
+    diff_n : int
+        Number of observations to use for accelerometer-derivative.
+    figsize : tuple[float, float]
+        Figure size in inches.
+    dpi : int
+        Figure DPI.
+
+    Returns
+    -------
+    matplotlib.pyplot.Figure
+    np.ndarray[matplotlib.pyplot.Axes]
+    """
+    assert len(spectrum) == len(df), 'Lengths does not match'
+    f, (ax, ax1, ax2) = plt.subplots(3, 1, figsize=figsize,
+                                     dpi=dpi, sharex=True)
+    df.drop('Cur', 1).diff(diff_n).plot.line(ax=ax)
+    df.Cur.plot.line(ax=ax1)
+    ax.set_title('d(Accelerometer)')
+    ax2.pcolorfast(spectrum.T, cmap='viridis')
+    ax1.set_title('Cur')
+    ax2.grid(False)
+    ax2.set_ylabel('Frequency (Hz)')
+    ax2.set_xlabel('Time')
+    ax2.set_title('Spectrogram')
+    f.tight_layout()
+
+    return f, np.array([ax, ax1, ax2])
+
+
+def plot_processed_airgard_df(df, line_cols, spectrum_cols, figure_kwargs=None,
+                         pcolorfast_kwargs=None, to_db=False):
+
+    n_plots = len(line_cols) + 1
+    figure_kwargs = figure_kwargs if figure_kwargs is not None else dict()
+    f, axes = plt.subplots(n_plots, 1, sharex=True,
+                           **figure_kwargs)
+
+    for ax, cols in zip(axes, line_cols):
+        df[cols].plot.line(ax=ax)
+
+    pcolorfast_kwargs = pcolorfast_kwargs if pcolorfast_kwargs is not None \
+        else dict()
+    if to_db:
+        spectrum = 10 * np.log10(df[spectrum_cols]).T
+    else:
+        spectrum = df[spectrum_cols].T
+
+    masked = np.ma.masked_where(np.isnan(spectrum), spectrum)
+    cmap = pcolorfast_kwargs.pop('cmap', 'viridis')
+    axes[-1].pcolorfast(masked, cmap=cmap, **pcolorfast_kwargs)
+    axes[-1].grid(False)
+
+    return f, axes
